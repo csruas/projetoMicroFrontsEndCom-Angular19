668cbedc6b4b2092ae357bee7c1395f16d1fc18b
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2026-02-13 18:07:18 -0300

    = Incluindo os modulos produtos grafico

--- vendas/webpack.config.js
+++ vendas/webpack.config.js
@@ -3,11 +3,16 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mfe1": "http://localhost:3000/remoteEntry.js",    
+    "produtos": "http://localhost:4001/remoteEntry.js",
+    "grafico": "http://localhost:4002/remoteEntry.js",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto'
+      }),
   },
 
 });
