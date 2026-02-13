323a17e3199a6cce2c165e8c8c4962c7f0597add
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2026-02-13 20:19:14 -0300

    =Adicionando o componente de produto

--- produtos/src/app/app.component.ts
+++ produtos/src/app/app.component.ts
@@ -1,11 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Produto {
+  nome: string;
+  valor: number;
+  imagem: string;
+}
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'produtos';
+
+  produtos: Produto[] = [
+    {
+      nome: 'Guitarra Tagima TG-530',
+      valor: 999.99,
+      imagem: 'https://picsum.photos/200/300/?blur'
+    },
+    {
+      nome: 'Dog Alemão',
+      valor: 500,
+      imagem: 'https://picsum.photos/id/237/200/300'
+    },
+    {
+      nome: 'Camera Canon EOS 5D Mark IV',
+      valor: 300.01,
+      imagem: 'https://picsum.photos/200'
+    },
+    {
+      nome: 'Cantera JBL Flip 5',
+      valor: 300.01,
+      imagem: 'https://picsum.photos/seed/picsum/200/300'
+    },
+  ];
 }
