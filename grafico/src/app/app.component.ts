748945f19384c818b3b075e8ec8f00930fde99b4
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2026-02-13 21:42:53 -0300

    Adicionando o dashboard de vendas

--- grafico/src/app/app.component.ts
+++ grafico/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'grafico';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const dados = [
+      { categoria: 'Eletronico', valor: 10},
+      { categoria: 'Roupas', valor: 15},
+      { categoria: 'Decoração', valor: 5},
+      { categoria: 'Ferramentas', valor: 30},
+
+    ];
+
+    const labels = dados.map(item => item.categoria);
+    const valores = dados.map(item => item.valor);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Vendas por Categoria',
+          data: valores,
+          backgroundColor: 'rgba(31, 82, 146, 0.6)'
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+  }
 }
